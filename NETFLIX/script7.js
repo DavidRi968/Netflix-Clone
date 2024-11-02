687e001a8b3b9422889d6e4cba448d8b700ef23d
David <davirdlessa@gmail.com>
2024-11-02 20:46:36 -0300

    Netflix clone

---
+++ NETFLIX/script7.js
@@ -0,0 +1,27 @@
+// Links que você vai colocar
+const linkTrailer = "https://www.youtube.com/live/JURh5vLVfI8?si=GKG5uSsMw_HAeOKj"; // substitua pelo link do trailer
+const linkInformacoes = "https://www.site.com/link-informacoes"; // substitua pelo link de informações
+
+// Referenciar os botões no JavaScript
+const botaoAssistir = document.getElementById("botao-assistir");
+const botaoInformacoes = document.getElementById("botao-informacoes");
+
+// Função de redirecionamento para o trailer
+botaoAssistir.addEventListener("click", function() {
+    window.open(linkTrailer, "_blank");
+});
+
+// Função de redirecionamento para mais informações
+botaoInformacoes.addEventListener("click", function() {
+    window.open(linkInformacoes, "_blank");
+});
+
+// Função para abrir a seção de informações
+function abrirInformacoes() {
+    document.getElementById('informacoes-jogo').style.display = 'flex';
+}
+
+// Função para fechar a seção de informações
+function fecharInformacoes() {
+    document.getElementById('informacoes-jogo').style.display = 'none';
+}
